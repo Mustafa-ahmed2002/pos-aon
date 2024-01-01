4b686312c5eda65077e35478a6eb4f4d312a137f
Mustafa Ahmed <mustafaahmed@Mustafas-Air-2.bbrouter>
2024-01-01 21:14:26 +0300

    final

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -5,7 +5,18 @@ export async function GET(req) {
   let categories = await prisma.category.findMany();
   return Response.json(categories);
 }
+export const dynamic = "force-dynamic"; // defaults to auto
 
+export async function GET(request) {
+  return new Response("Hello, Next.js!", {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
+}
 export async function POST(req) {
   const body = await req.json();
   try {
--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,6 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
+export const dynamic = "force-dynamic"; // defaults to auto
 
+export async function GET(request) {
+  return new Response("Hello, Next.js!", {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
+}
 export async function GET(req) {
   const searchParams = req.nextUrl.searchParams;
   const cat = searchParams.get("cat");

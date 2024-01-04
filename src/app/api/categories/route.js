ae23d393ed7e80a596208a51b672f3e7325f5e03
Mustafa Ahmed <mustafaahmed@Mustafas-Air-2.bbrouter>
2024-01-04 21:00:55 +0300

    updated

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -1,17 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-function setCorsHeaders(response) {
-  response.headers.set("Access-Control-Allow-Origin", "*");
-  response.headers.set(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  response.headers.set(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  return response;
-}
+
 export async function GET(req) {
   let categories = await prisma.category.findMany();
   return Response.json(categories);
